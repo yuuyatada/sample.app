af6ce2a67d367d28ca054cf333e22d896e4ca8dd
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-12-08 10:46:16 +0000

    4章終了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,14 +6,17 @@ class ListsController < ApplicationController
    
   end 
   
-  # 以下を追加
+  
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
+    
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    
+    redirect_to list_path(list.id)  
+    
   end
   
   def index
@@ -21,6 +24,7 @@ class ListsController < ApplicationController
   end 
   
   def show 
+   @list = List.find(params[:id])  
   end 
   
   def edit 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+ get 'lists/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
+  # .../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show', as: 'list'
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+end 

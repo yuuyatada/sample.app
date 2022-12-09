33c93d5538643662d7953f0a15580c4c1ffb2d9c
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-12-09 10:51:20 +0000

    8章終

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,17 +8,15 @@ class ListsController < ApplicationController
   
   
   def create
-    list = List.new(list_params)
+    @list = List.new(list_params)
     
-    list.save
-    
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    
-    redirect_to list_path(list.id)  
-    
+   if @list.save
+    redirect_to list_path(@list.id)  
+  else
+    render :new 
   end
-  
+end 
+
   def index
     @lists = List.all
   end 
@@ -38,6 +36,13 @@ def update
     redirect_to list_path(list.id) 
     
   end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト 
+    
+end 
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'top' => 'home#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  get 'top' => 'homes#top'
+  resources :lists
   
 end 
